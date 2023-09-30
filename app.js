14af548e50d349b02631b5728c5ce881012c2922
Mubarak Oladapo <oladapomubarak@gmail.com>
2023-09-30 12:50:17 +0100

    refactored search functionality

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
-//get elemet by id
-const main = document.getElementById("main");
+// selecting elements on the page
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-// get access to the api
+const main = document.getElementById("main");
 
-const getMovies = async (url) => {
+// getting the movies from the api
+async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.results);
-  showMovies(data.results);
-};
-
+  displayMovies(data.results);
+}
 getMovies(API_URL);
 
-function showMovies(movies) {
+function displayMovies(movies) {
   main.innerHTML = "";
+
   movies.forEach((movie) => {
     const { title, poster_path, vote_average, overview } = movie;
 
@@ -28,22 +27,23 @@ function showMovies(movies) {
     movieEl.classList.add("movie");
 
     movieEl.innerHTML = `
-      <img src = "${IMG_PATH + poster_path}" alt = "${title}" />
-      <div class = 'movie-info'>
-        <h3>${title}</h3>
-        <span class= '${giveClassByRate(vote_average)}'> ${vote_average} </span>
-      </div>
-      <div class= 'overview'>
-        <h3>Overview</h3>
+        <img src= " ${IMG_PATH + poster_path}" alt= "${title}" >
+        <div class = "movie-info">
+          <h3> ${title} </h3>
+          <span class = " ${checkRatings(
+            vote_average
+          )} "> ${vote_average} </span>
+        </div>
+        <div class = 'overview'>
+        <h3>Overview </h3>
         ${overview}
-      </div>
-      `;
-
+        </div>
+    `;
     main.appendChild(movieEl);
   });
 }
 
-function giveClassByRate(rate) {
+function checkRatings(rate) {
   if (rate >= 8) {
     return "green";
   } else if (rate >= 5) {
@@ -52,14 +52,25 @@ function giveClassByRate(rate) {
     return "red";
   }
 }
+
+// getting movies based on serach
+const hiddenSearch = document.querySelector(".hidden-search");
+const span = document.querySelector(".hidden-search span");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const searchTerm = search.value.trim();
-  console.log(searchTerm);
-  if (searchTerm && searchTerm !== "") {
-    getMovies(SEARCH_API + searchTerm);
+  const searchVal = search.value.trim();
+  if (searchVal && searchVal !== "") {
+    //search
+    // show search value to the user
+    span.textContent = searchVal;
+
+    hiddenSearch.classList.add("show-search");
+
+    getMovies(SEARCH_API + searchVal);
     search.value = "";
   } else {
     window.location.reload();
+    // search an not be empty
   }
 });
